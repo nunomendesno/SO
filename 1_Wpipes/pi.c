d1b8a6103200ac86881e7981e235f7b7f88b5718
TFMoreno <103313837+TFMoreno@users.noreply.github.com>
2022-04-23 10:05:53 +0100

    Add files via upload

--- 1_Wpipes/pi.c
+++ 1_Wpipes/pi.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
+#define numMinArguments (1)
+#define valTermsMin (10000000000)
 
 
 /*O novo programa (vector-processes) recebe através dos argumentos da linha comando o limite
 superior e o número de processos auxiliares a utilizar nos cálculos, i.e. para serem utilizados dois processos
 o programa deve ser executado da seguinte forma:
-
 ./vector-processes 1000 2
-
 *  O processo principal divide o cálculo em intervalos e envia-os aos processos filhos, i.e., se utilizarmos dois
 processos nos cálculos para um vetor de dimensão 1000, teríamos que o primeiro processo realiza os
 cáculos no intervalo entre 0 e 499 e o segundo processo entre 500 e 999. Neste caso deve tirar partido do
@@ -25,49 +26,95 @@ time ./vetor-processes 1000 2
 */
 
 
+bool checkNumberOfTerms(int argc){
+	
+	if(argc >= numMinArguments)
+		return 1;
+		
+	return 0;	
+}
+
+
+unsigned long int getNumberOfTerms(int argc, char** argv){
+	
+	unsigned long int NumberOfTerms;
+	
+	if (argc < 2){
+		
+		printf("Erro! Deve passar no argumento o número de termos a gerar.\n");
+		return -1;
+	}
+	
+	return NumberOfTerms = atoi(argv[1]);	
+}
+
+
+bool checkNumberOfTerms(unsigned long int NumberOfTerms){
+	
+	if(NumberOfTerms >= valTermsMin)
+		return 1;
+		
+	return 0;	
+}
+
+
+int getNumberOfProcesses(int argc, char** argv){
+	
+	int NumberOfProcesses;
+	
+	NumberOfProcesses = atoi(argv[2]);
+	
+	if (numProcesses < 1) {
+		
+		printf("Erro: Deverá pelo menos existir 1 processo para executar!\n");
+		return -1;
+	}
+	
+	return NumberOfProcesses;
+	
+}
+
+
+int getLimitInf ()
+
+
+
 
 double clacPi(char * argv){
 	long int n;
-	double sum=0.0;
-	double pi=0.0;
+	double sum = 0.0;
+	double pi = 0.0;
 	
-	n=atol(argv);	
-	if(n>= 10000000000)
+	n = atoi(argv);	
+	if(n>= valMin)
 	{
-		for(int k=1;k< n;++k)
+		for(int k = 1 ; k < n ; ++k)
 			sum+= pow(-1, k+1) / (2*k-1); 
 		 
 		pi = 4 * sum;
+		
 		printf("\nPI = %.10lf\n", pi);
 		return pi;
+		
 	}
-		else
-		{
-			printf("Numero de dermos devera ser superior a 10*10^9 e o valor inserido é %ld\n",n);	
-		}
-	
-	
-	
+	else
+	{
+		printf("Numero de dermos devera ser superior a 10*10^9 e o valor inserido é %ld\n",n);	
+	}
+		
 	return -1;
 	}
 
 
 double clacPiSun(char * argv , long nProcessos){
 	long int n;
-	double sum=0.0;
-	double pi=0.0;
+	double sum = 0.0;
+	double pi = 0.0;
 	
+	//n/nProcessos
 	
-	
-	n/nProcessos
-	
-	
-	
-	
-	
-	
-	n=atol(argv);	
-	if(n>= 10000000000)
+	n = atoi(argv);	
+	if(n >= valMin)
 	{
 		for(int k=1;k< n;++k)
 			sum+= pow(-1, k+1) / (2*k-1); 
@@ -76,77 +123,58 @@ double clacPiSun(char * argv , long nProcessos){
 		printf("\nPI = %.10lf\n", pi);
 		return pi;
 	}
-		else
-		{
-			printf("Numero de dermos devera ser superior a 10*10^9 e o valor inserido é %ld\n",n);	
-		}
 	
-	
+	else
+	{
+		printf("Numero de dermos devera ser superior a 10*10^9 e o valor inserido é %ld\n",n);	
+	}
 	
 	return -1;
-	}
+}
 
 
 
-int main(int argc, char** argv)
-{
+
+int main(int argc, char** argv) {
+	
+	
+	
+	
+	
 	
 	unsigned long int n;
-    unsigned int nChildren;
 
-    if (argc > 2) {
-        n = atol(argv[1]);
-        nChildren = atoi(argv[2]);
-        if (nChildren <1) {
-            printf("ERRO: É necessario pelo menos um processos filho!\n");
-            exit(1);
-            }
-    }
-    else {
-        printf(" Necessario pelo menos um processo para executar programa \n");
-        exit(1);
-    }
-	
-	
-	
+
 	int pipepi[2];
 	
 	pid_t parent = getpid();
 	
 	for (int i = 0; i < nChildren; i++) {
+		
         pipe(pipepi);											//cria pipe
         int retfork = fork();									//cria processo filho retfork
-        if (ret < 0) {
-            perror("Erro criar fork");
+        
+        if (retfork < 0) {
+            perror("Erro: Fork não foi criado\n");
             exit(1);
         }
         
-        if(retfork==0)										    // entra processo filho
-		{
+        if(retfork == 0) {
 			
-			
-			
-			
-			
-			
-			
+			// chama processo filho
 			
 		}
-			else{												// processo pai		
+		
+		else {														
 				
+			// chama processo pai
 				
-				
-				
-				}
+		}
 	
 	
 	}
 	
-	clacPi(argv[1]);
-
-	
-		
-	
+	//clacPi(argv[1]);
 	 
- return 0;
+	return 0;
 }
